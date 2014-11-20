ce43df23b1bd68e99f5b7ca388f5e50dacf5eb32
WilliamfShaw <WilliamfShaw@icloud.com>
2014-11-20 11:34:41 -0500

    fixed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,61 +2,61 @@
 #  profile picture seed
 #========================
 
-ProfilePicture.create (
+ProfilePicture.create ({
 	image_url: "http://www.merrickpubliclibrary.org/images/Dipiodocus.gif"
-	)
+	});
 
-ProfilePicture.create (
+ProfilePicture.create ({
 	image_url: "https://www.google.com/search?q=cute+dinosaur&es_sm=91&tbm=isch&tbo=u&source=univ&sa=X&ei=JxJuVK78OszdsASxw4HABA&ved=0CB8QsAQ&biw=1160&bih=581#facrc=_&imgdii=_&imgrc=X8HJMhIfYfDkHM%253A%3BE7iL8y9TNVyDRM%3Bhttp%253A%252F%252Fwww.sciencekids.co.nz%252Fimages%252Fpictures%252Fdinosaurs%252Ftriceratops%252Ftriceratopscartoon.jpg%3Bhttp%253A%252F%252Fwww.sciencekids.co.nz%252Fpictures%252Fdinosaurs%252Ftriceratops%252Ftriceratopscartoon.html%3B552%3B597"
-	)
+	});
 
 
-ProfilePicture.create (
+ProfilePicture.create ({
 	image_url: "http://images.clipartpanda.com/cute-dinosaur-9f906fb381d0224fcba70da266293035.png"
-	)
+	});
 
 
-ProfilePicture.create (
+ProfilePicture.create ({
 	image_url: "http://cdn10.mixrmedia.com/wp-uploads/wirebot/blog/2011/08/widget_ax823frnpayi3_hc2npkbv.jpg"
-	)
+	});
 
 
-ProfilePicture.create (
+ProfilePicture.create ({
 	image_url: "http://fc04.deviantart.net/fs70/f/2011/110/7/c/7ca5238ef52839011a05e1e67299e748-d3egfi6.jpg"
-	)
+	});
 
 
-ProfilePicture.create (
+ProfilePicture.create ({
 	image_url: "http://hottopic.scene7.com/is/image/HotTopic/10018960_hi?wid=1360"
-	)
+	});
 
 
-ProfilePicture.create (
+ProfilePicture.create ({
 	image_url: "http://imgs.tuts.dragoart.com/how-to-draw-a-sloth-for-kids_1_000000012109_5.gif"
-	)
+	});
 
 
-ProfilePicture.create (
+ProfilePicture.create ({
 	image_url: "http://www.lineboil.com/images/slth02.jpg"
-	)
+	});
 
 
-ProfilePicture.create (
+ProfilePicture.create ({
 	image_url: "http://images4.fanpop.com/image/photos/23000000/Cute-Ariel-disney-princess-23012684-211-239.jpg"
-	)
+	});
 
 
-ProfilePicture.create (
+ProfilePicture.create ({
 	image_url: "http://img3.wikia.nocookie.net/__cb20081013140955/ghostbusters/images/1/11/Bill_Murray.jpeg"
-	)
+	});
 
 
-ProfilePicture.create (
+ProfilePicture.create ({
 	image_url: "http://taro-kun.com/blog/wp-content/uploads/2011/09/8bit-Nick-Cage.jpg"
-	)
+	});
 
 
-ProfilePicture.create (
+ProfilePicture.create ({
 	image_url: "http://kleenradio.com/wp-content/uploads/2013/12/happy-cat_2818143.jpg"
-	)
+	});
 
