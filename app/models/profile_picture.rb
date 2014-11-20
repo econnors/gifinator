79fc8c208a0e1f907e102851db26fccf195dcbbd
WilliamfShaw <WilliamfShaw@icloud.com>
2014-11-20 11:25:48 -0500

    added profile picture seed and table

---
+++ app/models/profile_picture.rb
@@ -0,0 +1,5 @@
+class ProfilePicture < ActiveRecord::Base
+
+has_many :users
+
+end
--- db/migrate/20141119170141_create_users.rb
+++ db/migrate/20141119170141_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
     	t.string :username
     	t.text :bio
-    	t.text :profile_picture
+    	t.references :profile_picture
     	t.string :password_digest
 
     	t.timestamps
