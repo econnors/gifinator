c103e473ff2098b6fe2fd5dcb4a19b360392d4b9
itsmaggiemay <mmh332@nyu.edu>
2014-11-25 14:47:01 -0500

    new styles

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,77 +2,172 @@
 #  profile picture seed
 #========================
 
-ProfilePicture.delete_all
-
+# ProfilePicture.delete_all
 ProfilePicture.create ({
-	image_url: "https://c2.staticflickr.com/4/3214/2927523866_45a7e56ddb.jpg",
-	name: " 8-Bit Iron Man"
-	 });
-
-ProfilePicture.create ({
-	image_url: "http://fc00.deviantart.net/fs71/f/2012/290/8/4/8_bit_mario_recolor_by_rxlthunder-d5i4fs7.png",
+	image_url: "http://i.imgur.com/quvarMF.png",
 	name: "8-Bit Mario"
 	 });
 
-
 ProfilePicture.create ({
-	image_url: "http://img2.wikia.nocookie.net/__cb20140816030837/clubpenguinpookie/images/6/62/Bowser.jpg",
-	name: "8-Bit Bowser"
+	image_url: "http://i.imgur.com/Km1H6iG.png",
+	name: "8-Bit Luigi"
 	 });
 
 ProfilePicture.create ({
-	image_url: "http://fc02.deviantart.net/fs71/f/2011/342/b/c/8_bit_yoshi_by_toshirofrog-d4ijgaj.png",
-	name: "8-Bit Yoshi"
+	image_url: "http://i.imgur.com/lJ0qdPJ.png",
+	name: "8-Bit Wario"
 	 });
 
-
 ProfilePicture.create ({
-	image_url: "http://www.destructoid.com/elephant/ul/64048-468x-mega%20man.jpg",
-	name: "8-Bit MegaMan"
+	image_url: "http://i.imgur.com/lJ0qdPJ.png",
+	name: "8-Bit Waluigi"
 	 });
 
-
 ProfilePicture.create ({
-	image_url: "http://s00.yaplakal.com/pics/userpic/1/4/6/av-252641.png",
-	name: "8-Bit Ghost"
+	image_url: "http://i.imgur.com/dBQfak2.png",
+	name: "8-Bit Princess Peach"
 	 });
 
-
 ProfilePicture.create ({
-	image_url: "http://piq.codeus.net/static/media/userpics/piq_45959_400x400.png",
-	name: "8-Bit Link"
+	image_url: "http://i.imgur.com/WqMXJuV.png",
+	name: "8-Bit Princess Daisy"
 	 });
 
 
 ProfilePicture.create ({
-	image_url: "http://static.fjcdn.com/pictures/8_c81181_2986232.jpg",
-	name: "8-Bit Squirtle"
+	image_url: "http://i.imgur.com/rNMux6T.png",
+	name: "8-Bit Rosalina"
+	 });
+
+ProfilePicture.create ({
+	image_url: "http://i.imgur.com/LKCM8hk.png",
+	name: " 8-Bit Yoshi"
+	 });
+
+ProfilePicture.create ({
+	image_url: "http://i.imgur.com/jAmlKqn.png",
+	name: "8-Bit Wolverine"
 	 });
 
 
 ProfilePicture.create ({
-	image_url: "http://4.bp.blogspot.com/-PvOrCHHCg3o/UlPO4PUfEnI/AAAAAAAAAmE/APryyrh8rbU/s640/piq_24393_400x400.png",
-	name: "8-Bit Charizard"
+	image_url: "http://i.imgur.com/s0bCTMd.png",
+	name: "8-Bit TMNT"
 	 });
+
+ProfilePicture.create ({
+	image_url: "http://i.imgur.com/pzATZYe.png",
+	name: "8-Bit Thor"
+	 });
+
+ProfilePicture.create ({
+	image_url: "http://i.imgur.com/Hgr1Ubj.png",
+	name: "8-Bit Iron Man"
+	 });
+
+ProfilePicture.create ({
+	image_url: "http://i.imgur.com/RsTINBU.png",
+	name: "8-Bit Hulk"
+	 });
+
+ProfilePicture.create ({
+	image_url: "http://i.imgur.com/9JDoIoi.png",
+	name: "8-Bit Black Widow"
+	 });
+
+ProfilePicture.create ({
+	image_url: "http://i.imgur.com/7LKcT3C.png",
+	name: "8-Bit Captain America"
+	 })
 
 
 ProfilePicture.create ({
-	image_url: "http://piq.codeus.net/static/media/userpics/piq_42169_400x400.png",
-	name: "8-Bit Pikachu"
+	image_url: "http://i.imgur.com/apmZl40.png",
+	name: "8-Bit Batman"
 	 });
 
-
+ProfilePicture.create ({
+	image_url: "http://i.imgur.com/LlHhGQA.png",
+	name: "8-Bit Catwoman"
+	 });
 
 ProfilePicture.create ({
-	image_url: "http://ih0.redbubble.net/image.12985826.4531/sticker,375x360.png",
+	image_url: "http://i.imgur.com/KQcxLXc.png",
 	name: "8-Bit Bill Murray"
 	 });
 
-
 ProfilePicture.create ({
-	image_url: "http://nicolascage.us/wp-content/uploads/2013/08/8-Bit-Nic-Cage.jpg",
-	name: "8-Bit Nic"
+	image_url: "http://i.imgur.com/EREA2Yb.png",
+	name: "8-Bit Nicolas Cage"
 	 });
 
+ProfilePicture.create ({
+	image_url: "http://i.imgur.com/WTgfBqr.png",
+	name: "8-Bit Mr.T"
+	 });
 
+ProfilePicture.create ({
+	image_url: "http://i.imgur.com/NL8W9je.png",
+	name: "8-Bit Minion"
+	 });
+
+ProfilePicture.create ({
+	image_url: "http://i.imgur.com/csVljtD.png",
+	name: "8-Bit C-3PO"
+	 });
+
+ProfilePicture.create ({
+	image_url: "http://i.imgur.com/80otJ8J.png",
+	name: "8-Bit R2-D2"
+	 });
+
+ProfilePicture.create ({
+	image_url: "http://i.imgur.com/dXJMBtp.png",
+	name: "8-Bit Samus"
+	 });
+
+ProfilePicture.create ({
+	image_url: "http://i.imgur.com/zWuVQRX.png",
+	name: "8-Bit Goku Kamehameha"
+	 });
+
+ProfilePicture.create ({
+	image_url: "http://i.imgur.com/muyBX2L.png",
+	name: "8-Bit Contra"
+	 });
+
+ProfilePicture.create ({
+	image_url: "http://i.imgur.com/t4yyOGn.png",
+	name: "8-Bit Link"
+	 });
+
+ProfilePicture.create ({
+	image_url: "http://i.imgur.com/ziJceJ6.png",
+	name: "8-Bit Kirby"
+	 });
+
+ProfilePicture.create ({
+	image_url: "http://i.imgur.com/qM9zzLv.png",
+	name: "8-Bit Ash Ketchum"
+	 });
+
+ProfilePicture.create ({
+	image_url: "http://i.imgur.com/BhU6xil.png",
+	name: "8-Bit Pikachu"
+	 });
+
+ProfilePicture.create ({
+	image_url: "http://i.imgur.com/tlk44aj.png",
+	name: "8-Bit Bulbasaur"
+	 });
+
+ProfilePicture.create ({
+	image_url: "http://i.imgur.com/rSfNh1z.png",
+	name: "8-Bit Charmander"
+	 });
+
+ProfilePicture.create ({
+	image_url: "http://i.imgur.com/hr229gp.png",
+	name: "8-Bit Snorlax"
+	 });
 
